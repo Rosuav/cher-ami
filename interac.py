cea8f613f0e31a1c5f530b815a396bc4970c9b54
Chris Angelico <rosuav@gmail.com>
2020-03-18 17:04:33 +1100

    interac: Create a quick and easy way to grep the JSON file and pull up a tweet

--- interac.py
+++ interac.py
@@ -6,3 +6,14 @@ import json
 def t():
 	global tweet
 	tweet = json.loads(input("> "))
+
+def find(s):
+	with open("cher-ami.json") as f:
+		for line in f:
+			if not line.startswith("{"): continue
+			tw = json.loads(line)
+			txt = tw.get("full_text", tw.get("text", ""))
+			if s in txt:
+				print(tw["created_at"], "@" + tw["user"]["screen_name"])
+				global tweet
+				tweet = tw
